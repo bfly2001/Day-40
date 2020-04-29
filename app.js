332ed0c5e17b2adfaf2e375562b916910ddc02e2
bfly2001 <brendan.sflynn@gmail.com>
2020-04-29 14:52:45 -0700

    Update app.js
    made yellow square controllable by keyboard arrows

--- app.js
+++ app.js
@@ -1,3 +1,40 @@
+window.addEventListener("keydown", moveSomething, false);
+
+var deltaX = 0;
+var deltaY = 0;
+
+function moveSomething(e) {
+    switch(e.keyCode) {
+        case 37:
+            // left key
+            deltaX -= 2;
+            break;
+        case 38:
+            // up key
+            deltaY -= 2;
+            break;
+        case 39:
+            // right key
+            deltaX += 2;
+            break;
+        case 40:
+            // down key
+            deltaY += 2;
+            break;
+    }
+    e.preventDefault();
+
+    drawRectangle();
+}
+
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
-ctx.fillRect(20, 20, 150, 100);
+ctx.fillStyle = "yellow";
+
+function drawRectangle() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    //ctx.moveTo(200 + deltaX, 100 + deltaY);
+    ctx.fillRect(20 + deltaX, 20 + deltaY, 50, 50);
+}
+
+drawRectangle();
